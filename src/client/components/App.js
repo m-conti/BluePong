b686c86f2d57fe9f7eaed0b830e5f29396a474a9
Timothee BEHRA <tbehra@student.42.fr>
2019-08-17 13:13:27 +0200

    room component, UI button

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -4,6 +4,7 @@ import * as propTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 import Rooms from '../containers/Rooms/Rooms';
+import Room from '../containers/Room/Room';
 import Tetris from '../containers/Tetris/Tetris';
 
 
@@ -16,6 +17,7 @@ const app = ( props ) => {
 	return (
 		<div>
 			<Switch>
+				<Route exact path={'/room'} component={Room}/>
 				<Route path={'/rooms'} component={Rooms}/>
 				<Route exact path={'/'} component={Tetris}/>
 			</Switch>
---
+++ src/client/components/UI/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+import { Button } from '@material-ui/core';
+
+const style = {
+	root: {
+		background: '#AD0F10',
+		color: '#FBD806',
+	}
+}
+
+export default withStyles(style)(Button);
