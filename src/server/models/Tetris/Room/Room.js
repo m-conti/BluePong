ce0cb1040fe64f8658364e9bc45a75b6a5063e9d
matthieu CONTI <mconti@student.42.fr>
2019-08-19 00:46:42 +0200

    just one refresh at push item in rooms

--- src/server/models/Tetris/Room/Room.js
+++ src/server/models/Tetris/Room/Room.js
@@ -1,10 +1,10 @@
-import { remove, pick } from 'lodash';
-import { MAX_PLAYERS, MAX_VIEWERS } from '../../../../constants/tetris';
+import {remove, omit} from 'lodash';
+import {MAX_PLAYERS, MAX_VIEWERS} from '../../../../constants/tetris';
 
 import sockets from '../../Sockets/Sockets';
 
 class Room {
-	constructor( id, name, {maxPlayers, maxViewers} ) {
+	constructor(id, name, {maxPlayers, maxViewers}) {
 		this._id = id;
 		this.name = name;
 
@@ -25,13 +25,13 @@ class Room {
 
 	delete() {
 		console.log(`DELETE ROOM : ${this._id}`);
-		remove(sockets.tetris.rooms, ( r ) => r === this);
+		remove(sockets.tetris.rooms, (r) => r === this);
 	}
 
 	serialize() {
-		const obj = pick(this, ['_id', 'name', 'creationTime', 'endingTime', 'maxPlayers', 'maxViewers', 'isPlaying', 'isDone']);
-		obj.players = this.players.map(( player ) => player.serialize());
-		obj.viewers = this.viewers.map(( viewer ) => viewer.serialize());
+		const obj = omit(this, ['players', 'viewers', 'master']);
+		obj.players = this.players.map((player) => player.serialize());
+		obj.viewers = this.viewers.map((viewer) => viewer.serialize());
 		obj.master = this.master ? this.master.serialize() : null;
 		return obj;
 	}
@@ -40,11 +40,13 @@ class Room {
 	canJoin() {
 		return this.players.length < this.maxPlayers;
 	}
+
 	addPlayer(player) {
 		if (!this.canJoin()) throw new Error(`'${this.name}' can't accept mor players.`);
 		this.players.push(player);
 		if (!this.master) this.master = player;
 	}
+
 	removePlayer(player) {
 		remove(this.players, (p) => p === player);
 		if (!this.players.length) return this.delete();
--- src/server/models/Tetris/Tetris.js
+++ src/server/models/Tetris/Tetris.js
@@ -1,4 +1,4 @@
-import { remove } from 'lodash';
+import {remove} from 'lodash';
 
 import generateId from '../../../helpers/utilities/generateId';
 import Room from './Room/Room';
@@ -9,21 +9,24 @@ import * as actions from '../../actions/client/rooms';
 class Tetris {
 	constructor() {
 		this.rooms = new Proxy([], {
-			set: ( target, property, value ) => {
+			set: (target, property, value, receiver) => {
+				const old = target[property];
 				target[property] = value;
-				sockets.io.emit('action', actions.updateRooms(target));
+				if (typeof(value) === 'object') {
+					sockets.io.emit('action', actions.updateRooms(target));
+				}
 				return true;
 			},
 		});
 	}
 
-	getRoom( id ) {
-		const room = this.rooms.find(( room ) => room._id === id);
-		if ( !room ) throw new Error(`No room found with the given id : ${id}`);
+	getRoom(id) {
+		const room = this.rooms.find((room) => room._id === id);
+		if (!room) throw new Error(`No room found with the given id : ${id}`);
 		return room;
 	}
 
-	addRoom( roomOpt, player ) {
+	addRoom(roomOpt, player) {
 		const room = new Room(generateId(this.rooms), roomOpt.name, roomOpt);
 		player.join(room);
 		this.rooms.push(room);
