149f7a3b13706840ed14b356aa56c5f92d559e6c
m-conti <mc.heux@gmail.com>
2019-12-03 16:24:40 +0100

    game over one 1 player

--- src/params.js
+++ src/params.js
@@ -12,15 +12,15 @@ class Location {
 
 exports.location = {
 	server: new Location({
-		host: '10.11.3.6',
+		host: '0.0.0.0',
 		port: 3004,
 	}),
 	api: new Location({
-		host: '10.11.3.6',
+		host: '0.0.0.0',
 		port: 3005,
 	}),
 	client: new Location({
-		host: '10.11.3.6',
+		host: '0.0.0.0',
 		port: 8080,
 	})
 };
