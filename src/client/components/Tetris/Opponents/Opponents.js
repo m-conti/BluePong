0027dbb742605731f5364be5c99d5494d2b4c4c9
Timothée Behra <tbehra42@gmail.com>
2021-04-03 18:16:50 +0200

    Spectrum when 2 and 3 opponents

--- src/client/components/Tetris/Opponents/Opponents.js
+++ src/client/components/Tetris/Opponents/Opponents.js
@@ -3,15 +3,31 @@ import React from 'react';
 import classes from './Opponents.css';
 import Spectre from './Spectre/Spectre';
 
+const buildOpponents = ( opponents ) => {
+	if (opponents.length == 3) {
+		return (
+			[
+			<div>
+				<div className={classes.FirstTwo}>
+					<Spectre gameover={opponents[0].over} key={opponents[0].id} little={1} name={opponents[0].name} spectre={opponents[0].spectre} score={opponents[0].score}/>,
+					<Spectre gameover={opponents[1].over} key={opponents[1].id} little={1} name={opponents[1].name} spectre={opponents[1].spectre} score={opponents[1].score}/>,
+				</div>
+				<Spectre gameover={opponents[2].over} key={opponents[2].id} little={1} name={opponents[2].name} spectre={opponents[2].spectre} score={opponents[2].score}/>,
+			</div>
+			]
+		)
+	}
+	else {
+		return((opponents) ? opponents.map(o => (
+			[
+				<Spectre gameover={o.over} key={o.id} little={opponents.length > 1} name={o.name} spectre={o.spectre} score={o.score}/>,
+			]
+		)) : null);
+	}
+}
 
 const opponents = ( props ) => {
-
-	const opponents = (props.opponents) ? props.opponents.map(o => (
-		[
-			<Spectre gameover={o.over} key={o.id} little={props.opponents.length > 1} name={o.name} spectre={o.spectre} score={o.score}/>,
-		]
-	)) : null;
-
+	const opponents = buildOpponents(props.opponents);
 	return (
 		<div className={classes.Opponents}>
 			{opponents}
