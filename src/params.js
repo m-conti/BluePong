5147629987a9ff46e3db06ff82cdb3d3e1fc0a1b
matthieu CONTI <mconti@student.42.fr>
2021-04-08 12:30:49 +0200

    try server ip

--- src/params.js
+++ src/params.js
@@ -12,15 +12,15 @@ class Location {
 
 exports.location = {
 	server: new Location({
-		host: '0.0.0.0',
+		host: '51.178.49.196',
 		port: 3004,
 	}),
 	api: new Location({
-		host: '0.0.0.0',
+		host: '51.178.49.196',
 		port: 3005,
 	}),
 	client: new Location({
-		host: '0.0.0.0',
+		host: '51.178.49.196',
 		port: 8080,
 	})
 };
